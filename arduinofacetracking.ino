687f636239d38bb638911fa338a7ffa73ef49fb3
Joseph Lee <leejoseph2003@gmail.com>
2022-12-19 14:42:02 -0800

    fixed face recognition issue, added arduino script

---
+++ arduinofacetracking.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+int x;
+int y;
+
+Servo servoX;
+Servo servoY;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(1);
+  servoX.attach(5);
+  servoY.attach(6); 
+}
+
+void loop() {
+  while (!Serial.available()):
+    x = Serial.readString().toInt();
+    y = Serial.readString().toInt();
+  
+  servoX.write(x);
+  servoY.write(y);
+}
--- facetracking.py
+++ facetracking.py
@@ -1,21 +1,37 @@
-import cv2
-import os
-import keyboard
+import cv2, os, time, serial
+
+
 
 faceCascade = cv2.CascadeClassifier(os.path.join(cv2.data.haarcascades, 'haarcascade_frontalface_default.xml'))
 video_capture = cv2.VideoCapture(0)
 
+arduino = serial.Serial(port='COM4', baudrate=9600, timeout=.1)
+
+def arduino_write(x, y):
+    arduino.write(bytes(x, 'utf-8'))
+    arduino.write(bytes(y, 'utf-8'))
+
 while True:
-    _, frame = video_capture.read()
+    ret, frame = video_capture.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray, 1.1, 5)
     
+    height = frame.shape[0]
+    width = frame.shape[1]
+
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        centerx = (2 * x + w) / 2
+        centery = (2 * y + h) / 2
+
+        scalex = 
+
+
+
     cv2.imshow('Video', frame)
 
-    if keyboard.is_pressed('esc'):
+    if cv2.waitKey(1) == ord('q'):
         break
 
 video_capture.release()
